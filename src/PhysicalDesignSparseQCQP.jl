3a4b6ea180be698eb23a7598f9f98ef38a00a918
Pawel Latawiec <pawel@etiket.dev>
2023-04-22 20:42:28 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.33

---
+++ src/PhysicalDesignSparseQCQP.jl
@@ -0,0 +1,5 @@
+module PhysicalDesignSparseQCQP
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PhysicalDesignSparseQCQP
+using Test
+
+@testset "PhysicalDesignSparseQCQP.jl" begin
+    # Write your tests here.
+end
